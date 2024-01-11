8ed37f4de8c2a97d4fe6b435cb0366e853ce7d10
Gladesen <line@gladesen.dk>
2024-01-11 13:15:30 +0100

    Future-proofing for windows file structures

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,23 +4,23 @@ import glob
 
 
 def test_raw_data():
-    dir = _PATH_DATA + '/raw/'
+    dir = os.path.join(_PATH_DATA, "raw")
 
     # Checking the number of images
-    images = os.listdir(dir + 'images/')
+    images = os.listdir(os.path.join(dir, "images"))
     assert len(images) == 1584, "Incorrect number of images detected"
     # Checking if the train.csv file exists in the correct location
-    assert (os.path.exists(dir+'train.csv')), "Could not find the train.csv file"
+    assert (os.path.exists(os.path.join(dir,'train.csv'))), "Could not find the train.csv file"
 
 def test_data_loaded():
-    path = _PATH_DATA + '/processed/'
+    path = os.path.join(_PATH_DATA, 'processed')
 
     # Checking the labels
-    assert (os.path.exists(path+'labelled_training_data.csv')), "Could not find the labelled training data csv-file"
-    assert (os.path.exists(path+'labelled_test_data.csv')), "Could not find the labelled test data csv-file"
+    assert (os.path.exists(os.path.join(path,'labelled_training_data.csv'))), "Could not find the labelled training data csv-file"
+    assert (os.path.exists(os.path.join(path,'labelled_test_data.csv'))), "Could not find the labelled test data csv-file"
 
     # Checking the images
-    subfolders = [ f.name for f in os.scandir(path + 'TIMM/') if f.is_dir() ]
+    subfolders = [ f.name for f in os.scandir(os.path.join(path, 'TIMM')) if f.is_dir() ]
     assert len(subfolders) == 99, "Incorrect number of classes found."
     
     print(os.listdir(path))
